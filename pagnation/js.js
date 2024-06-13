eab515681eaebf531be7e99996ee50d91e7e484d
RockingWithTheBest <151398987+RockingWithTheBest@users.noreply.github.com>
2024-06-13 20:54:51 +0300

    succesful language translation for drinks menu

--- pagnation/js.js
+++ pagnation/js.js
@@ -35,9 +35,46 @@ firstButton .addEventListener('click', () => {
     showItem(currentIndex);
 });
 
-document.querySelector(".exit-btn").addEventListener('click', () => {
+document.querySelector(".exitbtn").addEventListener('click', () => {
   window.history.back();
 });
 
 
 const getValue = localStorage.getItem("Is_Language")
+var ArrayValues = {
+  "english":{
+    "black":"Black coffee",
+    "regularcapo":"Regular Cappucino",
+    "cappucinospeC":"Cappucino Special",
+    "china":"China Tea", 
+    "maffed":"Maffed Coffee",
+    "exitbtn":"Exit"
+  },
+  "russian":{
+    "black":"Черный чай",
+    "regularcapo":"Обычное каппучино",
+    "cappucinospeC":"Специал каппучино",
+    "china":"Китайский чай", 
+    "maffed":"Взбитый чай",
+    "exitbtn":"Выйти"
+  }
+}
+const value = document.querySelector(".black"); 
+
+console.log(getValue);
+if(getValue === "russian"){
+  value.textContent = ArrayValues["russian"].black;
+  document.querySelector(".exitbtn").textContent = ArrayValues["russian"].exitbtn;
+  document.querySelector(".regularcapo").textContent = ArrayValues["russian"].regularcapo;
+  document.querySelector(".cappucinospeC").textContent = ArrayValues["russian"].cappucinospeC;
+  document.querySelector(".china").textContent = ArrayValues["russian"].china;
+  document.querySelector(".maffed").textContent = ArrayValues["russian"].maffed;
+}
+else if (getValue ==="english"){
+  document.querySelector(".exitbtn").textContent = ArrayValues["english"].exitbtn;
+  document.querySelector(".black").textContent = ArrayValues["english"].black;
+  document.querySelector(".regularcapo").textContent = ArrayValues["english"].regularcapo;
+  document.querySelector(".cappucinospeC").textContent = ArrayValues["english"].cappucinospeC;
+  document.querySelector(".china").textContent = ArrayValues["english"].china;
+  document.querySelector(".maffed").textContent = ArrayValues["english"].maffed;
+}

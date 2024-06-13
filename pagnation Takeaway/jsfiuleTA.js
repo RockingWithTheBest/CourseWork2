628b61ee8a9529b305387927ba8d3fa6ad8c7215
RockingWithTheBest <151398987+RockingWithTheBest@users.noreply.github.com>
2024-06-13 09:52:10 +0300

    changed folder name from mTakeway to pagantion Takeaway

---
+++ pagnation Takeaway/jsfiuleTA.js
@@ -0,0 +1,40 @@
+const carouselItems = document.querySelectorAll('.carousel-item');
+const prevButton = document.querySelector('.carousel-prev');
+const nextButton = document.querySelector('.carousel-next');
+const lastButton = document.querySelector('.carousel-last');
+const firstButton = document.querySelector('.carousel-first');
+
+let currentIndex = 0;
+
+function showItem(index) {
+  carouselItems.forEach((item, i) => {
+    item.classList.toggle('active', i === index);
+  });
+}
+
+// Display the first item by default
+showItem(currentIndex);
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  showItem(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  showItem(currentIndex);
+});
+
+lastButton.addEventListener('click', () => {
+  currentIndex = carouselItems.length - 1;
+  showItem(currentIndex);
+});
+
+firstButton .addEventListener('click', () => {
+    currentIndex = 0;
+    showItem(currentIndex);
+});
+
+document.querySelector(".exit-btn").addEventListener('click', () => {
+  window.history.back();
+});
